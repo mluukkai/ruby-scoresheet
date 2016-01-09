4ee351b11d4ddc55f6e86dc18cde31b4f5d6f743
Chang <chang.rajani@gmail.com>
2016-01-10 00:08:13 +0200

    ExerciseFetcher::all should work now as well

--- lib/exercise_fetcher.rb
+++ lib/exercise_fetcher.rb
@@ -4,12 +4,43 @@ class ExerciseFetcher
     @auth = "#{ENV["USERNAME"]}:#{ENV["PASSWORD"]}"
   end
 
+  def find_users(exercises)
+      users = Array.new
+      Conf.exercises.downto 1 do |i|
+        users << exercises[i].keys
+      end
+      users.uniq
+    end
+
+  def find_exercises
+    exercises = Hash.new
+    Conf.exercises.downto 1 do |i|
+      exercises[i] = find_exercise_data i
+    end
+    exercises
+  end
+
+  def all
+    user_exercises = Hash.new
+    exercises = find_exercises
+    users = find_users exercises
+
+    Conf.exercises.downto 1 do |number|
+      exercises[number].each do |user, status|
+        user_exercises[user] = {} unless user_exercises[user]
+        user_exercises[user][number] = status
+      end
+    end
+
+    user_exercises
+  end
+
 
   def find_exercise_data(num)
-    exercise = "mluukkai/ruby-tehtava#{num}"
+    exercise = "#{Conf.repository}#{num}"
     travis_pulls = find_pulls_travis exercise
 
-    pull_users = pull_request_users pull_requests('mluukkai', "ruby-tehtava#{num}")
+    pull_users = pull_request_users pull_requests(get_user, get_repo(num))
     user_exercises(travis_pulls, pull_users)
   end
 
@@ -51,9 +82,20 @@ class ExerciseFetcher
     github.pull_requests.list
   end
 
+  def get_user
+    Conf.repository.split("\/").first
+  end
+
+  def get_repo(num)
+    Conf.repository.split("\/").second+num.to_s
+  end
+
   def status(pull_request)
     return "pass" if pull_request.include?('passed')
     return "testing" if pull_request.include?('started') or pull_request.include?('created')
     "fail"
   end
+
+  private :get_user, :get_repo, :status, :pull_requests
+
 end
